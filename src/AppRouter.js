80e480bea68f6c5543f789bd82f22781806e586c
Dawid Dluski <dawiddluski5@gmail.com>
2024-01-29 19:46:45 +0100

    dadany data folder oraz MealComponent

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Navigate, Routes } from 'react-router-dom';
 import LoginComponent from './components/login/LoginComponent';
 import FormComponent from './components/register/FormComponent';
+import MealComponent from './components/meal/MealComponent';
 // import HomeComponent from './path-to-components/HomeComponent';
 // import DayViewComponent from './path-to-components/DayViewComponent';
 import { useSelector } from 'react-redux';
@@ -29,6 +30,7 @@ const AppRouter = () => {
         {/* <PrivateRoute path="/home" component={HomeComponent} />
         <Route path="/day-view" component={DayViewComponent} /> */}
         <Route path='/' element={<LoginComponent />} exact />
+        <Route path='/meals' element={<MealComponent />} exact />
       </Routes>
     </Router>
   );
